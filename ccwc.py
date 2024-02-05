c284b52dd2eef0cb79db11e5af7c405b76ae9f44
Roman Barbalat <roman.barbalat22@gmail.com>
2024-02-05 17:40:57 -0500

    adjustments to the cl arg parser

--- ccwc.py
+++ ccwc.py
@@ -2,52 +2,63 @@ import os
 import argparse
 import sys
 
+#https://docs.python.org/3/library/argparse.html#core-functionality
 
-def ccwc(f_name, flag):
+
+def ccwc(f_name, c, l, w):
+
+    all = True if ((c and l and w) or (not c and not l and not w)) else False
+    c = c or all
+    l = l or all
+    w = w or all
 
     output = ""
 
     try:
         file = open(f_name)
+        file_stats = os.stat(f_name)
 
     except OSError:
         print(f_name + " can't be opened")
-        print("You must use the format ccwc.py filename OR ccww.py -flag filename")
-        print("Valid flags are -l, -c and -w (no more than one)")
         return
 
-    if flag == "-c" or not flag:
-        file_stats = os.stat(f_name)
-        output += str(file_stats.st_size)
-        if flag == "-c":
-            print(output + " " + f_name)
-            return
+    output += str(file_stats.st_size) + " bytes" if c else ""
 
-
-    lines = [line for line in file]
-    if flag == "-l":
-        print(str(len(lines)) + " " + f_name)
-        return
-    output += " " + str(len(lines))
+    if l or w:
+        lines = [line for line in file]
+        if l:
+            output += (" " + str(len(lines)) + " lines") if c else str(len(lines)) + " lines"
 
     words = 0
     for line in lines:
         words += len(line.split())
 
-    if flag == "-w":
-        print(str(words) + " " + f_name)
-        return
-    output += " " + str(words)
+    if w:
+        output += " " + str(words) + " words" if (c or l) else str(words) + " words"
 
     print(output + " " + f_name)
 
-#argv[0] is the name of the python script
-#print(sys.argv)
+#sys.argv is a list of the command line arguments
+#sys.argv[0] is the name of the python file
 
-flag = sys.argv[1] if len(sys.argv) > 2 else None
+parser = argparse.ArgumentParser(
+                    prog='wc_tool',
+                    description='My version of wc',
+                    epilog='Text at the bottom of help')
 
-if flag not in ["-c", "-l", "-w", None] or len(sys.argv) > 3:
-    print("The only acceptable flags are -c, -l and -w and you must provide at most one flag.")
+# parser.add_argument("-v", "--value")
+# action="store_true" for an optional flag that is present or absent (no value)
+parser.add_argument("-c", "--bytes", action="store_true")
+parser.add_argument("-l", "--lines", action="store_true")
+parser.add_argument("-w", "--words", action="store_true")
+
+parser.add_argument("filename", nargs="?")
+#nargs="?" makes the positional arg optional
+
+args = parser.parse_args()
+#print(args.bytes, args.lines, args.words, args.filename)
+
+if not args.filename:
+    print("You must provide a filename")
 else:
-    filename = sys.argv[-1]
-    ccwc(filename, flag)
+    ccwc(args.filename, args.bytes, args.lines, args.words)
